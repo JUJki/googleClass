de59260cbf7dc33391661d8119b975577a1ff218
Julien <julien@MacBook-Pro-de-Julien.local>
2019-07-15 01:32:32 +0200

    add const in Custom exception

--- CustomException.php
+++ CustomException.php
@@ -1,12 +1,12 @@
 <?php
 
-const GS_ERROR1 = 4000;
-const GS_ERROR2 = 4010;
+const GS_ERROR_400 = 4000;
+const GS_ERROR_401= 4010;
+const GS_ERROR_403= 4030;
+const GS_ERROR_404= 4040;
 
 class CustomException extends Exception {
-
-
-
+	
 	public function __construct($message, $code, Throwable $previous = null)
 	{
 		parent::__construct($message, $code, $previous);
